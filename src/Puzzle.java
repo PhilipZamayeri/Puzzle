1121b13d9ce2024602d34e027ae7bbb8029ac1a6
Robin Martinsson <martinsson.r@gmail.com>
2020-10-22 14:39:36 +0200

    Lägg till array, fix font och tom ruta.

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.lang.reflect.Array;
 
 /**
  * Created by Philip Zamayeri
@@ -11,7 +12,8 @@ import java.awt.*;
 public class Puzzle {
     JButton reset;
     JButton cheat;
-    String test = "Robin lägger till branch";
+    //String test = "Robin lägger till branch";
+    JButton[][] buttons = new JButton[4][4];
 
     private void gameDemo(){
         JFrame frame = new JFrame("Puzzle");
@@ -32,19 +34,35 @@ public class Puzzle {
         board.setLayout(new GridLayout(4, 4));
         Font fn = new Font("Tahoma", Font.BOLD, 60);
         int Num = 1;
+        int count = 1;
 
-        for (int row = 0; row < 4; row++) {
+        /*for (int row = 0; row < 4; row++) {
             for (int col = 0; col < 4; col++) {
                 if (Num == 16) {
                     break;
                 }
                 else {
-                    board.add(new JButton(String.valueOf(+Num))).setFont(fn);
+                    //board.add(new JButton(String.valueOf(+Num))).setFont(fn);
                 }
                 Num++;
             }
+        }*/
+
+        for (int i =0; i < buttons.length; i ++)
+            for (int j = 0; j < buttons[i].length; j++){
+                buttons[i][j] = new JButton("" + count++);
+                board.add(buttons[i][j]).setForeground(Color.black);
+                board.add(buttons[i][j]).setFont(fn);
         }
 
+        //JButton buttonEmpty = new JButton();
+          //  buttonPanel.add(buttonEmpty);
+        buttons[3][3].setBackground(null);
+        buttons[3][3].setFont(null);
+        buttons[3][3].setText(null);
+        buttons[3][3].setOpaque(true);
+
+        buttonPanel.setBackground(new Color(255-255-153));
         frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
