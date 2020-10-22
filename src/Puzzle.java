7e93f6d75b7b62ad8adf8a73b592fa6d2817440d
Philip Zamayeri <Philip.Zamayeri@gmail.com>
2020-10-22 15:26:13 +0200

    Initial commit changed to a multidimensionell array added mouseListener

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Created by Philip Zamayeri
@@ -9,40 +11,77 @@ import java.awt.*;
  * Copyright: MIT
  */
 public class Puzzle {
-    JButton reset;
+    JButton newGame;
     JButton cheat;
+    JButton[][] button = new JButton[4][4];
+    public static final Color buttonColor = new Color(0,51,102);
+
 
     private void gameDemo(){
+        try {
+            UIManager.setLookAndFeel( UIManager.getCrossPlatformLookAndFeelClassName() );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        MouseAdapter mouseAdapter = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        };
+
+        MouseAdapter mouseAdapter2 = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        };
+
+        MouseAdapter mouseAdapter3 = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        };
+
+
         JFrame frame = new JFrame("Puzzle");
         JPanel board = new JPanel();
         JPanel buttonPanel = new JPanel();
         JPanel panel  = new JPanel();
+        Font fn = new Font("Tahoma", Font.BOLD, 60);
 
-        reset = new JButton("Nytt spel");
+
+
+        newGame = new JButton("Nytt spel");
         cheat = new JButton("Cheat");
+        newGame.addMouseListener(mouseAdapter2);
+        cheat.addMouseListener(mouseAdapter3);
 
         panel.setLayout(new BorderLayout());
         panel.add(buttonPanel, BorderLayout.NORTH);
         panel.add(board);
 
-        buttonPanel.add(reset);
+        buttonPanel.add(newGame);
         buttonPanel.add(cheat);
 
-        board.setLayout(new GridLayout(4, 4));
-        Font fn = new Font("Tahoma", Font.BOLD, 60);
-        int Num = 1;
+        int counter = 1;
 
-        for (int row = 0; row < 4; row++) {
-            for (int col = 0; col < 4; col++) {
-                if (Num == 16) {
-                    break;
-                }
-                else {
-                    board.add(new JButton(String.valueOf(+Num))).setFont(fn);
-                }
-                Num++;
+        board.setLayout(new GridLayout(4, 4));
+        for (int i = 0; i < button.length; i++)
+            for (int j = 0; j < button[i].length; j++){
+                button[i][j] = new JButton(""+ counter++);
+                board.add(button[i][j]).setForeground(Color.white);
+                button[i][j].setFont(fn);
+                button[i][j].setBackground(buttonColor);
+                button[i][j].addMouseListener(mouseAdapter);
             }
-        }
+        button[3][3].setBackground(buttonColor);
+        button[3][3].setOpaque(true);
+        button[3][3].setText(null);
+
+
 
         frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
